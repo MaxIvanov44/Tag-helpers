cbd359876860f6a4a6b456712d54307687e268cc
MaxIvanov44 <Lvanovmaksim200033@gmail.com>
2020-01-30 17:50:55 +0300

    Initial Commit

---
+++ Tag helpers/Tag helpers/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Tag_helpers.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
